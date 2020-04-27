60d9b2ab7b45e2edbd9e8e880123a7259a164976
Felix Wang <felix.wang@oocl.com>
2020-04-27 19:18:54 +0800

    init project

---
+++ src/test/java/com/oocl/felix/RatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.felix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
